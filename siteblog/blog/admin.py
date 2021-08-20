85ffa68669e6e29632efe8fca276d60557cfce8d
egor <egor555550@mail.ru>
2021-08-20 18:19:30 +0500

    lesson 5

--- siteblog/blog/admin.py
+++ siteblog/blog/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
+from .models import *
 # Register your models here.
+from django import forms
+from ckeditor.widgets import CKEditorWidget
+
+
+
+class PostAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorWidget())
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title', )}
+    form = PostAdminForm
+
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title', )}
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title', )}
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Post, PostAdmin)
--- siteblog/siteblog/urls.py
+++ siteblog/siteblog/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += path('__debug__/', include(debug_toolbar.urls)),
