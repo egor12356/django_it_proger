1f995a21a2a65fa7aa4aba492dfd35c68a68dc2a
egor <egor555550@mail.ru>
2021-08-22 15:37:32 +0500

    lesson 11

--- siteblog/blog/views.py
+++ siteblog/blog/views.py
@@ -29,3 +29,20 @@ def get_category(requests, slug):
 
 def get_post(requests, slug):
     return render(requests, 'blog/category.html')
+
+
+class PostByCategory(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 2
+    allow_empty = False  # Ошибка 404 при запросе несуществующей категории
+
+
+    def get_queryset(self):
+        return Post.objects.filter(category__slug=self.kwargs['slug'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Category.objects.get(slug=self.kwargs['slug'])
+        return context
