7890631a65dfb102a1bf716ddfc3dfd9cd5e3da9
egor <egor555550@mail.ru>
2021-08-22 14:22:14 +0500

    lesson 9

--- siteblog/blog/models.py
+++ siteblog/blog/models.py
@@ -46,6 +46,9 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'slug': self.slug})
+
     class Meta:
         ordering = ['title']
         verbose_name = 'Статья(ю)'
--- siteblog/blog/views.py
+++ siteblog/blog/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import *
 
 # Create your views here.
 
+
+
+class Home(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 2
+
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Classic Blog Design'
+        return context
 
 def index(requests):
     return render(requests, 'blog/index.html')
 
 
 
-def get_category(requests):
+def get_category(requests, slug):
+    return render(requests, 'blog/category.html')
+
+
+def get_post(requests, slug):
     return render(requests, 'blog/category.html')
